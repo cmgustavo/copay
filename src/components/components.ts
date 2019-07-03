1785013b8613206080d4f4a344f4894c6d8cdbfa
Gabriel Masclef <gabrielmasclef@hotmail.com>
2019-07-03 13:46:35 -0300

    Remove unnecessary code

--- src/components/components.ts
+++ src/components/components.ts
@@ -9,7 +9,6 @@ import { LabelTip } from './label-tip/label-tip';
 import { PriceCard } from './price-card/price-card';
 import { PriceChart } from './price-card/price-chart/price-chart';
 import { SearchBarComponent } from './search-bar/search-bar';
-import { WalletGroupSelectorComponent } from './wallet-group-selector/wallet-group-selector';
 
 export const COMPONENTS = [
   ActionCardComponent,
@@ -22,6 +21,5 @@ export const COMPONENTS = [
   ClipboardCardPage,
   SearchBarComponent,
   PriceCard,
-  PriceChart,
-  WalletGroupSelectorComponent
+  PriceChart
 ];
